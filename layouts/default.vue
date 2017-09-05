0ce6e3ab65b3d9de71eb7f4dfb40cce013b96cf2
Albert Chen <albert.cht@gmail.com>
2017-09-05 11:19:16 +0800

    add outline for develop

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,15 @@
 <template lang="pug">
   b-container(:fluid="true")
     b-row
-      b-col(cols="4")
+      b-col(cols="2")
         nav
-      b-col(cols="8")
+      b-col(cols="10")
         header
         nuxt
 </template>
+
+<style>
+  *:hover {
+    outline: 1px solid rgba(255, 0, 0, .5);
+  }
+</style>
