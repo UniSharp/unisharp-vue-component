15d3258d5b2a19b665c8eb660205eb7b00ff13de
ChenCatherine <catherine@unisharp.com>
2017-09-05 10:32:21 +0800

    project init

---
+++ nuxt.config.js
@@ -0,0 +1,51 @@
+const StylelintPlugin = require('stylelint-webpack-plugin')
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'blue-admin',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'A base template' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Global css
+  */
+  css: [
+    { src: '~/assets/scss/main.scss', lang: 'scss' }
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLINT and STYLELINT on save
+    */
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+
+        config.plugins.push(new StylelintPlugin({
+          files: ['**/*.scss', '**/*.vue'],
+          syntax: 'scss'
+        }))
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  .container.d-flex.flex-column.align-items-center.justify-content-center
+    logo
+    .caption.my-3.text-center
+      h1.display-3.text-dark blue-admin
+      h2.display-4.text-muted A base template
+    .links
+      a.btn.btn-lg.btn-outline-success.px-4.mr-3(href="https://nuxtjs.org/", target="_blank") Documentation
+      a.btn.btn-lg.btn-outline-dark.px-4(href="https://github.com/nuxt/nuxt.js", target="_blank") GitHub
+</template>
+
+<script>
+  import Logo from '~/components/Logo.vue'
+
+  export default {
+    components: {
+      Logo
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    height: 100vh;
+  }
+</style>
