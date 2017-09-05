f475112c3cc423c76278a58cefc73668f47dd45a
ChenCatherine <catherine@unisharp.com>
2017-09-05 13:02:30 +0800

    Add nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,7 +1,56 @@
 <template lang="pug">
-  h1 Nav
+  .u-nav
+    header
+      .backend-logo
+        a.img-logo(href="#"): img(src="../assets/img/logo_img.png")
+    nav
+      ul
+        li(v-b-toggle.collapse-manager)
+          i.fa.fa-file-text-o(aria-hidden="true")
+          span 管理員管理
+        b-collapse#collapse-manager(tag="ul")
+          li: a(href="#") 管理員列表
+          li: a(href="#") 新增管理員
+        li(v-b-toggle.collapse-member)
+          i.fa.fa-file-text-o(aria-hidden="true")
+          span 會員管理
+        b-collapse#collapse-member(tag="ul")
+          li: a(href="#") 會員列表
+          li: a(href="#") 新增會員
+        li(v-b-toggle.collapse-setting)
+          i.fa.fa-file-text-o(aria-hidden="true")
+          span 全站設定
 </template>
 
-<style>
+<script>
+  export default {
+    //
+  }
+</script>
 
+<style lang="scss" scoped>
+  ul li {
+    list-style: none;
+  }
+
+  .u-nav {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background: #3c4350;
+    left: 0;
+    top: 0;
+
+    header {
+      .backend-logo {
+        width: 100%;
+        padding: 30px 15px;
+
+        a.img-logo img {
+          width: 170px;
+          height: auto;
+        }
+      }
+    }
+  }
 </style>
