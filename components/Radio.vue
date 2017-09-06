24d4efecebf224d939a2230a91de095c90008bfe
ChenCatherine <catherine@unisharp.com>
2017-09-06 17:00:13 +0800

    Add radio

---
+++ components/Radio.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+  .u-radio(:class="{ 'custom-controls-stacked': stacked }")
+    label.custom-control.form-check-inline.custom-radio(:key="option.value", v-for="(option, index) in options")
+      input.custom-control-input(
+        type="radio",
+        :name="name",
+        :value="option.value",
+        :checked="defaultValue == option.value",
+        :disabled="disabled ? disabled : option.disabled",
+        :required="required",
+        @change="$emit('change')",
+        @focus="$emit('focus')",
+        @blur="$emit('blur')",
+        @click="$emit('click')"
+      )
+      span.custom-control-indicator
+      span.custom-control-description {{ option.text }}
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        //
+      }
+    },
+    props: {
+      options: {
+        default: () => {
+          return [
+            { text: 'Option1', value: 1, disabled: false },
+            { text: 'Option2', value: 2, disabled: true },
+            { text: 'Option3', value: 3, disabled: false }
+          ]
+        }
+      },
+      name: {
+        required: true,
+        default: null
+      },
+      defaultValue: {
+        default: 1
+      },
+      disabled: {
+        default: false
+      },
+      required: {
+        default: false
+      },
+      stacked: {
+        default: false
+      }
+    }
+  }
+</script>
--- pages/form.vue
+++ pages/form.vue
@@ -30,8 +30,16 @@
         b-form-group(label='Textarea:', label-for='exampleInput5')
           b-form-textarea#exampleInput5(placeholder='Enter something', :rows='3', :max-rows='6')
         //- Radio
-        b-form-group(label='Food:', label-for='exampleInput6')
-          b-form-radio#exampleInput6(:options='foods', v-model='form.food')
+        .form-group.row
+          label.col-md-2
+            span.title Radio Inline：
+          .col-md-10
+            u-radio(name="food4", :options="foods", defaultValue="3")
+        .form-group.row
+          label.col-md-2
+            span.title Radio Stacked：
+          .col-md-10
+            u-radio(name="food5", :options="foods", stacked="true")
         //- Button
         button.btn.btn-primary(type="button") Submit
         button.btn.btn-default(type="reset") Reset
@@ -43,9 +51,10 @@
   import UBreadcrumb from '~/components/Breadcrumb'
   import UCheckbox from '~/components/Checkbox'
   import USwitch from '~/components/Switch'
+  import URadio from '~/components/Radio'
 
   export default {
-    components: { UAdmin, UBreadcrumb, UCheckbox, USwitch },
+    components: { UAdmin, UBreadcrumb, UCheckbox, USwitch, URadio },
     data () {
       return {
         form: {
@@ -66,6 +75,18 @@
     methods: {
       onSubmit (e) {
         alert(JSON.stringify(this.form))
+      },
+      handleChange () {
+        console.log('change')
+      },
+      handleFocus () {
+        console.log('focus')
+      },
+      handleBlur () {
+        console.log('blur')
+      },
+      handleClick () {
+        console.log('click')
       }
     }
   }
