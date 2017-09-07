deb43d0f006ca6055b5d5ba2e9a8ab51454cb013
Albert Chen <albert.cht@gmail.com>
2017-09-07 11:37:17 +0800

    抽掉form展示頁所有的bootstrap-vue元件

--- pages/form.vue
+++ pages/form.vue
@@ -1,64 +1,69 @@
 <template lang="pug">
   u-admin
     u-breadcrumb(slot="breadcrumb")
-    b-card(header="Form Components", header-tag="header")
-      b-form(@submit.prevent='onSubmit')
-        //- Label
-        .form-group.row
-          label.col-md-2 Label：
-          .col-md-10.form-control-static I'm label text.
-        //- Input
-        .form-group.row
-          label.col-md-2.col-form-label Email address：
-          .col-md-10
-            input.form-control(type='email', v-model='form.email', required='', placeholder='Enter email')
-        u-select(v-model="form.food", :options="foods")
-        //- Select
-        b-form-group(label='Food:', label-for='exampleInput3')
-          b-form-select#exampleInput3(:options='foods', required='', v-model='form.food')
-        //- Checkbox
-        .form-group.row
-          label.col-md-2.col-form-label Checkbox：
-          .col-md-10.form-inline
-            u-checkbox(v-model="check", value="yes")
-              | Option 1
-        //- Checkboxes
-        .form-group.row
-          label.col-md-2.col-form-label Checkbox：
-          .col-md-10.form-inline
-            u-checkbox(v-model="checks", value="1")
-              | Option 1
-            u-checkbox(v-model="checks", value="2")
-              | Option 2
-            u-checkbox(v-model="checks", value="3")
-              | Option 3
-        //- Switch
-        .form-group.row
-          label.col-md-2.col-form-label Switch：
-          .col-md-10
-            u-switch(name='switch', v-model='form.checked', @change='showSwitchValue')
-        //- Textarea
-        b-form-group(label='Textarea:', label-for='exampleInput5')
-          b-form-textarea#exampleInput5(placeholder='Enter something', :rows='3', :max-rows='6')
-        //- Radio
-        .form-group.row
-          label.col-md-2
-            span.title Radio Inline：
-          .col-md-10
-            u-radio(name="food1", :options="foods", v-model="form.radio.value")
-        .form-group.row
-          label.col-md-2
-            span.title Radio Stacked：
-          .col-md-10
-            u-radio(name="food2", :options="foods", stacked="true", v-model="form.radio.value")
-        .form-group.row
-          label.col-md-2
-            span.title Radio Inline：
-          .col-md-10
-            input(type="text", :value="showRadioText(form.radio.value)")
-        //- Button
-        button.btn.btn-primary(type="button") Submit
-        button.btn.btn-default(type="reset") Reset
+    .card
+      .card-header Form Components
+      .card-body
+        form
+          //- Label
+          .form-group.row
+            label.col-md-2 Label：
+            .col-md-10.form-control-static I'm label text.
+          //- Input
+          .form-group.row
+            label.col-md-2.col-form-label Email address：
+            .col-md-10
+              input.form-control(type='email', v-model='form.email', required='', placeholder='Enter email')
+          //- Select
+          .form-group.row
+            label.col-md-2.col-form-label Select：
+            .col-md-10
+              u-select(v-model="form.food", :options="foods")
+          //- Checkbox
+          .form-group.row
+            label.col-md-2.col-form-label Checkbox：
+            .col-md-10.form-inline
+              u-checkbox(v-model="check", value="yes")
+                | Option 1
+          //- Checkboxes
+          .form-group.row
+            label.col-md-2.col-form-label Checkbox：
+            .col-md-10.form-inline
+              u-checkbox(v-model="checks", value="1")
+                | Option 1
+              u-checkbox(v-model="checks", value="2")
+                | Option 2
+              u-checkbox(v-model="checks", value="3")
+                | Option 3
+          //- Switch
+          .form-group.row
+            label.col-md-2.col-form-label Switch：
+            .col-md-10
+              u-switch(name='switch', v-model='form.checked', @change='showSwitchValue')
+          //- Textarea
+          .form-group.row
+            label.col-md-2.col-form-label Textarea：
+            .col-md-10
+             textarea.form-control(rows="5")
+          //- Radio
+          .form-group.row
+            label.col-md-2
+              span.title Radio Inline：
+            .col-md-10
+              u-radio(name="food1", :options="foods", v-model="form.radio.value")
+          .form-group.row
+            label.col-md-2
+              span.title Radio Stacked：
+            .col-md-10
+              u-radio(name="food2", :options="foods", stacked="true", v-model="form.radio.value")
+          .form-group.row
+            label.col-md-2
+              span.title Radio Inline：
+            .col-md-10
+              input(type="text", :value="showRadioText(form.radio.value)")
+          //- Button
+          button.btn.btn-primary(type="button") Submit
+          button.btn.btn-default(type="reset") Reset
   </template>
 
 <script>
