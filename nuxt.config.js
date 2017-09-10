810159dd0f06b31a7a70e50541c61df2187169d0
Sasaya <storyn26383@gmail.com>
2017-09-11 01:40:53 +0800

    modify title

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'blue-admin',
+    title: 'Blue Admin',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
