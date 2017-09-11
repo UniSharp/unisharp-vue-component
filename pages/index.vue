bdfc5bc44d787354a7577a716f8e8d1ceb6c688e
Sasaya <storyn26383@gmail.com>
2017-09-11 16:47:35 +0800

    card

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,20 @@
   u-admin
     u-breadcrumb(slot="breadcrumb")
     .container.d-flex.flex-column.align-items-center.justify-content-center
-      .caption.my-3.text-center
-        h1.display-3.text-dark Blue Admin
-        h2.display-4.text-muted A base template
-      .links
-        a.btn.btn-lg.btn-outline-success.px-4.mr-3(href="https://nuxtjs.org/", target="_blank") Documentation
-        a.btn.btn-lg.btn-outline-dark.px-4(href="https://github.com/nuxt/nuxt.js", target="_blank") GitHub
+      .card.w-100
+        h4.card-header Featured
+        .card-body
+          h4.card-title Special title treatment
+          p.card-text With supporting text below as a natural lead-in to additional content.
+          a.btn.btn-secondary(href="#") Go somewhere
+          a.btn.btn-primary(href="#") Go somewhere
+      .card.w-100
+        .card-body
+          h4.card-title Card title
+          h6.card-subtitle.mb-2.text-muted Card subtitle
+          p.card-text Some quick example text to build on the card title and make up the bulk of the card's content.
+          a.card-link(href="#") Card link
+          a.card-link(href="#") Another link
 </template>
 
 <script>
@@ -20,5 +28,7 @@
 </script>
 
 <style lang="scss" scoped>
-  //
+  .card + .card {
+    margin-top: 2rem;
+  }
 </style>
