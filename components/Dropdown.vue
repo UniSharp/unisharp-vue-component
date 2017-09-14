ae1f4aa94ca4a0f7241a71d104d08fa482a0af49
Sasaya <storyn26383@gmail.com>
2017-09-14 16:13:50 +0800

    fix dropdown animation

--- components/Dropdown.vue
+++ components/Dropdown.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
   .u-dropdown(:class="{ active }")
-    .u-dropdown-backdrop(@click="active = false")
+    .u-dropdown-backdrop(v-if="active", @click="active = false")
     .u-dropdown-toggle(@click.prevent="active = true")
       slot(name="toggle")
-    slot
+    transition(name="fade")
+      slot(v-if="active")
 </template>
 
 <script>
@@ -27,19 +28,34 @@
   }
 </script>
 
+<style lang="scss">
+  .u-dropdown {
+    .dropdown-menu.fade-enter-active, .dropdown-menu.fade-leave-active {
+      transition: opacity .2s ease;
+
+      .dropdown-item {
+        transition: transform .2s cubic-bezier(.25, .8, .5, 1);
+      }
+    }
+
+    .dropdown-menu.fade-enter, .dropdown-menu.fade-leave-to {
+      opacity: 0;
+
+      .dropdown-item {
+        transform: translateY(-10px);
+      }
+    }
+  }
+</style>
+
 <style lang="scss" scoped>
   .u-dropdown-backdrop {
-    display: none;
     position: fixed;
     top: 0;
     left: 0;
     width: 100%;
     height: 100vh;
     z-index: 10000;
-
-    .active & {
-      display: block;
-    }
   }
 
   .u-dropdown-toggle {
@@ -52,21 +68,6 @@
     left: inherit;
     top: 100%;
     display: block;
-    opacity: 0;
-    transition: opacity .3s ease;
-
-    .dropdown-item {
-      transform: translateY(-10px);
-      transition: transform .3s cubic-bezier(.25, .8, .5, 1);
-    }
-
-    .active & {
-      z-index: 10002;
-      opacity: 1;
-
-      .dropdown-item {
-        transform: translateY(0);
-      }
-    }
+    z-index: 10002;
   }
 </style>
