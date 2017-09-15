2c8486f146ac7269c5524465b9e7b8e1f1f6adc0
Sasaya <storyn26383@gmail.com>
2017-09-15 18:07:47 +0800

    basic login(not finished yet)

--- pages/login.vue
+++ pages/login.vue
@@ -1,36 +1,34 @@
 <template lang="pug">
   .wrapper
-    .container-bg
-      header
-        u-logo(to="#", :width="parseInt(200)")
-        h1.m-0 客製化網站內容管理系統
+    header
+      u-logo.mb-2(to="/login", :width="200")
+      h1.mb-0.title 客製化網站內容管理系統
 
-      div.main
-        .card
-          .card-header
-            h2 師大十年計畫
-          .card-body
-            form(@submit.prevent="onSubmit")
-              .form-group.row
-                .input-group
-                  span#name.input-group-addon: i.fa.fa-user
-                  input.form-control(type="text", name="name", v-model="form.name", placeholder="帳號", aria-describedby="name")
-              .form-group.row
-                .input-group
-                  span#password.input-group-addon: i.fa.fa-lock
-                  input.form-control(type="password", name="password", v-model="form.password", placeholder="密碼", aria-describedby="password")
-              .form-group.row
-                button.btn.btn-default.w-100(type="submit") 登入
-              .form-group.row
-                a(href="#") 忘記密碼
+    main
+      .card
+        h3.card-header BLUE ADMIN
+        .card-body
+          form(@submit.prevent="onSubmit")
+            .form-group
+              .input-group
+                span#name.input-group-addon: i.fa.fa-user
+                input.form-control(type="text", name="name", v-model="form.name", placeholder="帳號", aria-describedby="name")
+            .form-group
+              .input-group
+                span#password.input-group-addon: i.fa.fa-lock
+                input.form-control(type="password", name="password", v-model="form.password", placeholder="密碼", aria-describedby="password")
+            .form-group
+              button.btn.btn-primary.btn-block.btn-lg(type="submit") 登入
+            .form-group.mb-0
+              nuxt-link(to="/login") 忘記密碼
 
-      footer
-        p.mb-0 BACKEND - Content Management System
-        p.mb-0
-          span Made with
-          i.fa.fa-heart
-          span by
-          a(href="http://www.unisharp.com", target="_blank") UniSharp
+    footer
+      p.mb-1 BACKEND - Content Management System
+      p.mb-0
+        span Made with&nbsp;
+        i.fa.fa-heart
+        span &nbsp;by&nbsp;
+        a(href="http://www.unisharp.com", target="_blank") UniSharp
 </template>
 
 <script>
@@ -58,83 +56,54 @@
   @import "~assets/scss/_variables";
 
   .wrapper {
-    width: 100%;
-    height: 100vh;
-    position: relative;
+    height: 100%;
+    background: url(~assets/img/login-bg.jpg) no-repeat center center;
+    background-size: cover;
 
-    .container-bg {
-      margin: 0;
-      height: 100%;
-      background: url(~assets/img/login-bg.jpg) no-repeat center center;
-      background-size: cover;
-      position: relative;
+    header, footer {
+      position: fixed;
+      width: 100%;
+      background-color: $sidebar-bg;
+      padding: 1.75rem 0;
+      text-align: center;
+      z-index: 1;
+    }
 
-      header {
-        position: absolute;
-        top: 0;
-        width: 100%;
-        background: $indigo;
-        padding: 30px 0;
-        text-align: center;
+    header {
+      top: 0;
 
-        h1 {
-          text-align: center;
-          color: rgba(255, 255, 255, .8);
-          font-weight: 300;
-          font-size: .813rem;
-          letter-spacing: 8px;
-          text-indent: 10px;
-          padding: 10px 0 0;
+      .title {
+        color: rgba(255, 255, 255, .8);
+        font-weight: 300;
+        font-size: .813rem;
+        letter-spacing: .5rem;
+        text-indent: .5rem;
+      }
+    }
+
+    main {
+      height: 100vh;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      .card {
+        min-width: 25rem;
+
+        .card-header {
+          background-color: darken($gray-100, 2.5%);
+          border-bottom: 0;
         }
       }
+    }
 
-      div.main {
-        width: 100%;
-        height: 100%;
-        display: -ms-flexbox;
-        display: flex;
-        -ms-flex-align: center;
-        align-items: center;
-        -ms-flex-pack: center;
-        justify-content: center;
-        padding: 0;
+    footer {
+      bottom: 0;
 
-        .card {
-          position: inherit;
-
-          .card-header {
-            padding: 15px 35px;
-          }
-
-          .card-body {
-            padding: 15px 35px;
-          }
-        }
-      }
-
-      footer {
-        position: absolute;
-        bottom: 0;
-        width: 100%;
-        background: $indigo;
-        padding: 30px 0;
-        text-align: center;
-
-        p {
-          font-size: .75rem;
-          letter-spacing: 1px;
-          color: rgba(255, 255, 255, .4);
-          text-align: center;
-          padding: 2px 0;
-
-          i {
-            padding: 0 5px;
-          }
-
-          a {
-            padding: 0 0 0 3px;
-          }
-        }
+      p {
+        font-size: .75rem;
+        letter-spacing: .05rem;
+        color: rgba(255, 255, 255, .4);
       }
     }
   }
