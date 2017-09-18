1b7a8f1da79d404a9f69d75d916a79a01e34b668
Sasaya <storyn26383@gmail.com>
2017-09-18 15:33:20 +0800

    menu from config

---
+++ config.js
@@ -0,0 +1,49 @@
+export default {
+  menu: [{
+    icon: 'file-text-o',
+    title: 'Style Guide',
+    children: [{
+      title: 'Colors',
+      to: '/guide/colors'
+    }, {
+      title: 'Buttons',
+      to: '/guide/buttons'
+    }, {
+      title: 'Typography',
+      to: '/guide/typography'
+    }, {
+      title: 'Card',
+      to: '/guide/card'
+    }, {
+      title: 'Table',
+      to: '/guide/table'
+    }, {
+      title: 'Alerts',
+      to: '/guide/alerts'
+    }]
+  }, {
+    icon: 'file-text-o',
+    title: '元件範例',
+    children: [{
+      title: '表單元件',
+      to: '/form'
+    }, {
+      title: '表格',
+      to: '/table'
+    }]
+  }, {
+    icon: 'file-text-o',
+    title: '會員管理',
+    children: [{
+      title: '會員列表',
+      to: ''
+    }, {
+      title: '新增會員',
+      to: ''
+    }]
+  }, {
+    icon: 'file-text-o',
+    title: '全站設定',
+    to: ''
+  }]
+}
