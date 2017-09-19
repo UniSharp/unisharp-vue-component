e2c1a7f406f186e09a24a26d9ac43283001184d3
Sasaya <storyn26383@gmail.com>
2017-09-19 22:22:57 +0800

    modify title font size

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -2,7 +2,7 @@
   .u-welcome.container.d-flex.flex-column.align-items-center.justify-content-center
     u-face.smile.mt-5.mb-4
     h2.h1 歡迎登入
-    h1.mb-3 {{ config.index.title }}
+    h1.h2.mb-3 {{ config.index.title }}
     p << 請從左側目錄選擇要管理的功能
 </template>
 
