01539e0e3b887f68578a3dadc72dbb4b738c68c9
Sasaya <storyn26383@gmail.com>
2017-09-19 16:21:26 +0800

    logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
-  nuxt-link.u-logo(:to="to")
-    img(src="../assets/img/logo.png", :width="width")
+  nuxt-link.u-logo.d-flex.align-items-center.justify-content-center(:to="to")
+    .logo
+    .text.d-flex.flex-column.justify-content-center
+      h1 BACKEND
 </template>
 
 <script>
@@ -11,21 +13,64 @@
       to: {
         type: String,
         default: () => config.index.to
-      },
-      width: {
-        type: Number,
-        default: 170
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-  .u-logo {
-    display: block;
+  @import "../assets/scss/variables";
 
-    img {
-      height: auto;
+  $logo-color: #7ac5c1;
+  $logo-size: 2rem;
+  $square-size: $logo-size * .35;
+  $trangle-size: $logo-size * .28;
+  $square-position: ($logo-size - $square-size) / 2;
+  $trangle-position: ($logo-size - $trangle-size) / 2;
+
+  a {
+    &:hover {
+      text-decoration: none;
     }
+  }
+
+  .logo {
+    position: relative;
+    width: $logo-size;
+    height: $logo-size;
+    border-radius: 50%;
+    background-color: $logo-color;
+
+    &:before {
+      content: "";
+      position: absolute;
+      width: $square-size;
+      height: $square-size;
+      top: $square-position;
+      left: $square-position;
+      background-color: #3c4350;
+    }
+
+    &:after {
+      content: "";
+      position: absolute;
+      width: 0;
+      height: 0;
+      top: $trangle-position;
+      right: $square-position;
+      border-top: $trangle-size / 2 solid transparent;
+      border-bottom: $trangle-size / 2 solid transparent;
+      border-right: $trangle-size / 2 solid $logo-color;
+    }
+  }
+
+  h1 {
+    margin-bottom: 0;
+    margin-left: .5rem;
+    font-size: $logo-size - .5rem;
+    font-weight: bold;
+    font-family: Arial;
+    letter-spacing: .1rem;
+    color: $white;
   }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,6 @@
 <template lang="pug">
   u-admin
     .container.d-flex.align-items-center.justify-content-center
-      .logo
-      .text.d-flex.flex-column.justify-content-center
-        h1 BLUE
-        h1 ADMIN
 </template>
 
 <script>
@@ -14,50 +10,6 @@
 </script>
 
 <style lang="scss" scoped>
-  $logo-size: 5.5rem;
-  $square-size: $logo-size * .34375;
-  $trangle-size: $logo-size * .28125;
-  $square-position: ($logo-size - $square-size) / 2;
-  $trangle-position: ($logo-size - $trangle-size) / 2;
-
-  .logo {
-    position: relative;
-    width: $logo-size;
-    height: $logo-size;
-    border-radius: 50%;
-    background-color: #7ac5c1;
-
-    &:before {
-      content: "";
-      position: absolute;
-      width: $square-size;
-      height: $square-size;
-      top: $square-position;
-      left: $square-position;
-      background-color: #e1e9eb;
-    }
-
-    &:after {
-      content: "";
-      position: absolute;
-      width: 0;
-      height: 0;
-      top: $trangle-position;
-      right: $square-position;
-      border-top: $trangle-size / 2 solid transparent;
-      border-bottom: $trangle-size / 2 solid transparent;
-      border-right: $trangle-size / 2 solid #7ac5c1;
-    }
-  }
-
-  h1 {
-    margin-bottom: 0;
-    margin-left: 1.5rem;
-    font-size: 2.5rem;
-    letter-spacing: .2rem;
-    font-weight: 500;
-  }
-
   .container {
     margin: -1.5rem auto;
     height: calc(100vh - 153px);
