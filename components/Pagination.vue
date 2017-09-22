fd07575ed4295c89c9aeff2d70ea22c9a3fbe71b
昱行 <laiyushing@unisharp.com>
2017-09-22 09:38:39 +0800

    修正 pagination 過長問題

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -4,22 +4,36 @@
     ul.pagination.mb-0
       //- previous button
       li.page-item(:class="{ disabled: isActive(1) }")
-        a.page-link(aria-label="Previous", @click.stop.prevent="setPage(currentPage - 1)")
+        a.page-link(aria-label="Previous", @click.stop.prevent="setPage(1)")
           span(aria-hidden="true"): i.fa.fa-angle-double-left
+          span.sr-only First
+      li.page-item(:class="{ disabled: isActive(1) }")
+        a.page-link(aria-label="Previous", @click.stop.prevent="setPage(currentPage - 1)")
+          span(aria-hidden="true"): i.fa.fa-angle-left
           span.sr-only Previous
       //- page button
+      li.page-item(v-show="!isOverBottom && isOverLimit")
+        a.page-link ...
       li.page-item(v-for="page in pageList", :class="{ active: isActive(page) }", :key="page")
         span.page-link(v-if="isActive(page)") {{ page }}
           span.sr-only (Current)
         a.page-link(@click.stop.prevent="setPage(page)", v-else) {{ page }}
+      li.page-item(v-show="!isOverTop && isOverLimit")
+        a.page-link ...
       //- next button
-      li.page-item(:class="{ disabled: isActive(pageList) }")
+      li.page-item(:class="{ disabled: isActive(totalPage) }")
         a.page-link(aria-label="Next", @click.stop.prevent="setPage(currentPage + 1)")
-          span(aria-hidden="true"): i.fa.fa-angle-double-right
+          span(aria-hidden="true"): i.fa.fa-angle-right
           span.sr-only Next
+      li.page-item(:class="{ disabled: isActive(totalPage) }")
+        a.page-link(aria-label="Next", @click.stop.prevent="setPage(totalPage)")
+          span(aria-hidden="true"): i.fa.fa-angle-double-right
+          span.sr-only Last
 </template>
 
 <script>
+  import _ from 'lodash'
+
   export default {
     props: {
       totalRows: {
@@ -38,13 +52,9 @@
         type: String,
         default: ''
       },
-      prevText: {
-        type: String,
-        default: '&laquo;'
-      },
-      nextText: {
-        type: String,
-        default: '&raquo;'
+      limitPage: {
+        type: Number,
+        default: 7
       }
     },
     data () {
@@ -53,7 +63,32 @@
       }
     },
     computed: {
+      totalPage () {
+        return Math.ceil(this.totalRows / this.perPage)
+      },
+      isOverLimit () {
+        return this.totalPage > this.limitPage
+      },
+      isOverTop () {
+        return (this.totalPage - this.currentPage) <= Math.floor(this.limitPage / 2)
+      },
+      isOverBottom () {
+        return (this.currentPage - 1) <= Math.floor(this.limitPage / 2)
+      },
       pageList () {
+        if (this.isOverLimit) {
+          if (this.isOverTop) {
+            return _.range(this.totalPage - this.limitPage + 2, this.totalPage + 1)
+          }
+
+          if (this.isOverBottom) {
+            return _.range(1, this.limitPage)
+          }
+
+          let end = this.currentPage + Math.floor(this.limitPage / 2) - 1
+          return _.range(end - this.limitPage + 3, end + 1)
+        }
+
         return Math.ceil(this.totalRows / this.perPage)
       }
     },
