b50530133cddaf6814781808695c3a60d02cdf38
布魯 <blue@unisharp.com>
2017-10-12 15:59:58 +0800

    add component

---
+++ components/InputBtn.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  .u-input-btn(@click="click")
+    .u-select
+      .u-select-current.form-control.placeholder 複數篩選
+</template>
+
+<script>
+  export default {
+    methods: {
+      click (e) {
+        this.$emit('click', e)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .u-input-btn {
+    cursor: pointer;
+
+    .u-select {
+      min-width: auto;
+
+      .u-select-current {
+        &:after {
+          margin-right: 0;
+          border: none;
+        }
+      }
+    }
+  }
+</style>
