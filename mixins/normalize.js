86454a4803c2bbf707956906583f7fd93f9a48e3
Albert Chen <albert.cht@gmail.com>
2018-02-26 14:41:16 +0800

    fix normalize value

--- mixins/normalize.js
+++ mixins/normalize.js
@@ -10,7 +10,7 @@ export default {
       // [ 'text', 'text' ] # it will use text as value
       return _.map(options, (v, k) => {
         return {
-          value: v.value || v.text || v || k || null,
+          value: typeof v.value !== 'undefined' ? v.value : v.text || v || k || null,
           text: _.toString(v.text || v),
           disabled: v.disabled || false
         }
