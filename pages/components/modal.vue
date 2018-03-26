fa6e09eb2465c1bf57a156c3f5ce2b46dc4b1c98
Stream <stream@unisharp.com>
2018-03-26 15:42:23 +0800

    add demo code section to model and pagination page

--- pages/components/modal.vue
+++ pages/components/modal.vue
@@ -1,8 +1,55 @@
 <template lang="pug">
   u-admin
     button.btn.btn-primary(type="button", @click.prevent.stop="$refs.modal.show()") Launch Modal
+
+    md.pt-3.
+      ```
+      button.btn.btn-primary(type="button", @click.prevent.stop="$refs.modal.show()") Launch Modal
+
+      u-modal(ref="modal")
+        span(slot="title") Modal
+        button.btn.btn-info(slot="actions", @click.stop.prevent="$refs.modal.hide()") NO
+        button.btn.btn-primary(slot="actions", @click.stop.prevent="$refs.modal.hide()") YES
+        p Hello World
+        p Hello World
+        p Hello World
+        p Hello World
+        p.mb-0 Hello World
+      ```
+
     button.btn.btn-primary(type="button", @click.prevent.stop="$refs.modalSm.show()") Launch Small Modal
+
+    md.pt-3.
+      ```
+      button.btn.btn-primary(type="button", @click.prevent.stop="$refs.modalSm.show()") Launch Small Modal
+
+      u-modal(ref="modalSm", size="sm")
+        span(slot="title") Small Modal
+        button.btn.btn-info(slot="actions", @click.stop.prevent="$refs.modalSm.hide()") NO
+        button.btn.btn-primary(slot="actions", @click.stop.prevent="$refs.modalSm.hide()") YES
+        p Hello World
+        p Hello World
+        p Hello World
+        p Hello World
+        p.mb-0 Hello World
+      ```
+
     button.btn.btn-primary(type="button", @click.prevent.stop="$refs.modalLg.show()") Launch Large Modal
+
+    md.pt-3.
+      ```
+      button.btn.btn-primary(type="button", @click.prevent.stop="$refs.modalLg.show()") Launch Large Modal
+
+      u-modal(ref="modalLg", size="lg")
+        span(slot="title") Large Modal
+        button.btn.btn-info(slot="actions", @click.stop.prevent="$refs.modalLg.hide()") NO
+        button.btn.btn-primary(slot="actions", @click.stop.prevent="$refs.modalLg.hide()") YES
+        p Hello World
+        p Hello World
+        p Hello World
+        p Hello World
+        p.mb-0 Hello World
+      ```
 
     u-modal(ref="modal")
       span(slot="title") Modal
@@ -34,9 +81,3 @@
       p Hello World
       p.mb-0 Hello World
 </template>
-
-<script>
-  export default {
-    //
-  }
-</script>
--- pages/components/pagination.vue
+++ pages/components/pagination.vue
@@ -1,12 +1,7 @@
 <template lang="pug">
   u-admin
     u-pagination(:totalRows="12345", :perPage="500")
+
+    md.pt-3.
+      u-pagination(:totalRows="12345", :perPage="500")
 </template>
-
-<script>
-
-</script>
-
-<style lang="scss" scoped>
-
-</style>
