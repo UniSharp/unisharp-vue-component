64c04e236d9ea9b2932531363cf234643d1b85c8
ChenCatherine <catherine@unisharp.com>
2018-03-26 15:46:00 +0800

    checkbox basic usage

--- pages/components/checkbox-radio.vue
+++ pages/components/checkbox-radio.vue
@@ -1,8 +1,58 @@
 <template lang="pug">
   u-admin
+    form.card.w-100
+      .card-body
+        .form-group.row
+          label.col-sm-2.col-form-label.text-sm-right Checkbox:
+          .col-sm-10
+            u-checkbox-group(v-model="form.checked", :options="options")
+        md.
+          ```
+          <template lang="pug">
+            u-checkbox(v-model="check", value="yes")
+            | Option 1
+          </template>
+          <script>
+            export default {
+              data () {
+                return {
+                  form: {
+                    checked: true
+                  },
+                  options: [{
+                    text: 'I',
+                    value: 1
+                  }, {
+                    text: 'II',
+                    value: 2
+                  }, {
+                    text: 'III',
+                    value: 3
+                  }]
+                }
+              }
+            }
+          </script>
+          ```
 </template>
 <script>
   export default {
-    //
+    data () {
+      return {
+        form: {
+          checked: true
+        },
+        options: [{
+          text: 'I',
+          value: 1
+        }, {
+          text: 'II',
+          value: 2
+        }, {
+          text: 'III',
+          value: 3
+        }]
+      }
+    }
   }
 </script>
